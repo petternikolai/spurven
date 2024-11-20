eb4032d1b29c99b3c11d2225569b10f1db914a9c
petternikolai <petkri14832@stud.noroff.no>
2024-11-20 14:53:14 +0100

    Refactor Header and Hjem components for improved layout and styling; update Vite configuration for build optimization

--- src/pages/Hjem.jsx
+++ src/pages/Hjem.jsx
@@ -8,18 +8,16 @@ export default function Hjem() {
       style={{ backgroundImage: `url(${bakgrunn})` }}
     >
       <div className="mx-auto max-w-7xl">
-        <div className="z-10 lg:w-full">
-          <div className="flex items-center px-6 py-16 sm:py-20 lg:px-8 lg:pr-0 custom-height">
-            <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl bg-white/90 p-6 rounded-lg">
-              <h1 className="text-pretty text-xl font-medium tracking-tight sm:text-4xl md:text-5xl">
-                Spurven er en pålitelig samarbeidspartner, levende opptatt av å
-                sikre våre kunder gode resultater
-              </h1>
-              <div className="mt-10 flex items-center gap-x-6">
-                <Link to="/om-oss" className="text-sm/6 font-semibold ">
-                  Lær mer <span aria-hidden="true">→</span>
-                </Link>
-              </div>
+        <div className="flex items-center px-6 py-16 sm:py-20 lg:px-8 custom-height">
+          <div className="max-w-2xl lg:mx-0 lg:max-w-5xl bg-white/90 xl:bg-transparent p-6 rounded-lg">
+            <h1 className="text-pretty text-xl font-medium tracking-tight sm:text-5xl">
+              Spurven er en pålitelig samarbeidspartner, levende opptatt av å
+              sikre våre kunder gode resultater
+            </h1>
+            <div className="text-sm lg:text-lg mt-10 flex items-center gap-x-6">
+              <Link to="/om-oss" className="font-semibold ">
+                Lær mer <span aria-hidden="true">→</span>
+              </Link>
             </div>
           </div>
         </div>
