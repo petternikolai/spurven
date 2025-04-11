3b3c64d2bd2aded2cca484d8ffcb1d6745152b6b
Petter-Nikolai Kristoffersen <pnkristoffersen@gmail.com>
2025-04-11 13:20:46 +0200

    Automatisk henting av nåværende år i footer.

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-white">
       <div className="flex justify-center items-center py-4">
         <div className="flex font-thin gap-1 text-center text-sm">
-          <p className="font-sans">&copy; </p>
-          <p>2024 Spurven AS</p>
+          <p className="font-sans">&copy;</p>
+          <p>{currentYear} Spurven AS</p>
           <p>|</p>
           <Link to="/personvern" className="underline ml-1">
             Personvern
